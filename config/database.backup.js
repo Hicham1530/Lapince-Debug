083588f130ce2f8404b0ef38b085ea62edaf2a26
Hicham Guelzim <hicham.guelzim@gmail.com>
2025-07-28 00:22:38 +0200

    Relancer déploiement pour test DB

--- config/database.backup.js
+++ config/database.backup.js
@@ -3,14 +3,26 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL || {
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  dialect: "postgres",
-  logging: false,
-});
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    logging: false,
+  });
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      dialect: "postgres",
+      logging: false,
+    }
+  );
+}
+
 
 const testConnection = async () => {
   try {
